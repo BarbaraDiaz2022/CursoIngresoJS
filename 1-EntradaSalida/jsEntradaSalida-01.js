ae509c34153cf1f6499169863707fa03efab0844
BarbaraDiaz2022 <barbydiaz2000@gmail.com>
2022-02-12 23:39:51 -0300

    cambios nuevos

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -7,12 +7,7 @@ function mostrar()
 {
 /*	alert("Esto funciona de maravilla");
 
-
-
-
 //Diaz Barbara
-
-
 Se piden tres nombre de producto, y los precios de cada  producto ingresado, sacar el precio bruto 
 (la suma de los tres sin impuestos), el promedio de los precios y el precio final total más iva (21%), 
 pedir un porcentaje de descuento y aplicarlo al precio final, mostrar todos los datos calculados e ingresados por alert.
@@ -27,7 +22,7 @@ pedir un porcentaje de descuento y aplicarlo al precio final, mostrar todos los 
 	//mostrar los datos calculados e ingresados por alerta, solo una alerta
 	//pedir datos por prompt 
 */
-
+/*
 	let ingresoNombre1; 
 	let ingresoNombre2;
 	let ingresoNombre3;
@@ -56,5 +51,50 @@ pedir un porcentaje de descuento y aplicarlo al precio final, mostrar todos los 
 	precioFinal= precioMasIva + porcentajeDescuento;
 	
 	alert("El precio es " + precioFinal); 
+*/
+/*Supongamos que para un cometa se necesitan 25 cm para la interseccion AB y 10 para DC
+La superficie de un romboide se saca con la formula (D1 * D2)/2 (siendo D diagonal uno en este caso AB y D2
+	seria la diagonal dada por DC). Con esa formula sacamos la cantidad de papel necesario para el cometa, si queremos
+	sacar la cantidad de papel total tenemos que multiplicar por un 10% adicional (usado para la cola).
+	Para saber los metros de varillas necesarios para 10 cometas tenemos que multiplicar esos 25 cm y 10 cm por 10 (la
+	respuesta va a estar dada en centimetros y para pasala a metros tenemos que multiplicar por 0.01 (que es la
+	relacion entre centimetros y metros porque 1cm--->0.01 mts)*/
+/*
+	let pidoAB;
+	let pidoDC; 
+	let area; 
+	let cantidadTotal;
+	let cantidadDeVarilla; 
+	let pasajeDeCmAMts; 
+	
+	pidoAB= prompt("Ingrese valor 1");
+	pidoDC= prompt("Ingrese valor 2");
+	area= (pidoAB * pidoDC) / 2; 
+	cantidadTotal= area + (area * 10/100);
+	alert("necesitará " + cantidadTotal + " de papel");
 
+	cantidadDeVarilla= (pidoAB + pidoDC) * 10;
+	pasajeDeCmAMts= cantidadDeVarilla * 0.01; 
+	alert("necesitará " + pasajeDeCmAMts + " metros de plastico"); */
+
+	/* para el triangulo bicolor necesito la mitad del papel total dividido 2 y sumado, cada
+	mitad corresponde a un color */
+	 let pidoAB;
+	let pidoDC; 
+	let area; 
+	let cantidadTotal;
+	let buscoCadaMitad;
+	let cantidadDeVarilla; 
+	let pasajeDeCmAMts; 
+	
+	pidoAB= prompt("Ingrese valor 1");
+	pidoDC= prompt("Ingrese valor 2");
+	area= (pidoAB * pidoDC) / 2; 
+	cantidadTotal= area + (area * 10/100);
+	buscoCadaMitad= cantidadTotal / 2;
+	alert ("necesitará " + buscoCadaMitad + " de papel rosa y " + buscoCadaMitad + " de papel azul");
+
+	cantidadDeVarilla= (pidoAB + pidoDC) * 10;
+	pasajeDeCmAMts= cantidadDeVarilla * 0.01; 
+	alert("necesitará " + pasajeDeCmAMts + " metros de plastico"); 	
 }
--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,16 +1,76 @@
-//Diaz Barbara
-//Ejercicio if 7
+//Diaz Barbara 
+//Ejercicio 7 Bis V3
 
 function mostrar()
-{
+{	/*
 	let edad; 
 	let estadoCivil;
-	
 	edad = parseInt(document.getElementById("txtIdEdad").value); 
 	estadoCivil = document.getElementById("estadoCivil").value; 
-
+	
 	if ( edad <= 17 && estadoCivil != "Soltero"){
 		alert("Es muy pequeño para NO ser soltero"); 
-	}
-	
+	} */
+	let edad;
+	let mensaje;
+	let resto;
+	let nombre; 
+	let estadoCivil;
+
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+	resto= edad % 2;
+	nombre= prompt("Ingrese su nombre");
+	estadoCivil= document.getElementById("estadoCivil").value; 
+
+		if (edad <=12){
+			mensaje= "feliz día";
+			if (nombre == "Ricardo"){ //ejercicio 3 f
+				mensaje= mensaje + " , muy chiquito para ese nombre";
+			}	
+		}
+			else{
+				if(edad <17){
+					mensaje= "usted es adolescente";
+						if (nombre == "Violeta"){ //ejercio 3 e
+							mensaje= mensaje + " , como el color!" ; 
+						}
+						if (estadoCivil== "Casado"){ //ejercicio 3 h
+							mensaje= mensaje + " casada quiere casa";
+						}
+						if (estadoCivil== "Divorciado"){ //ejercicio 3 j
+							mensaje= mensaje + " ,toda una vida por delante"; 
+						}
+				}
+				if (edad ==17){
+					mensaje= "usted es adolescente " + ", ultimo año!"; 
+				}
+					if (estadoCivil== "Divorciado"){
+						mensaje= mensaje + " ,toda una vida por delante"; 
+					}
+				if (edad >=18){
+					mensaje= "tienes edad de laburar,";
+						if (estadoCivil== "Soltero"){
+							mensaje= mensaje + " a salir";//ejercicio 3 i
+						}
+				}
+				if (edad ==33){
+					mensaje= "tienes edad de laburar" + " , como cristo";
+						if (estadoCivil== "Soltero"){
+							mensaje= mensaje + ", a salir"; //ejercicio 3 i
+						}
+				}
+				if (edad ==60){
+					mensaje= " a jubilarse";
+					if (nombre == "Alfredo"){ //ejercicio 3 g 
+						mensaje=  mensaje + " , como el de QUEENS!,  " ; 
+					}
+				}
+				if (edad ==88){
+					mensaje= "lindo numero"; 
+				}		
+			}
+			if (resto ===0){
+				mensaje= mensaje + " ,sos par!"; 
+			}			
+	alert(mensaje);
 }
